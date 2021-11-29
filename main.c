063ad6bca286d608fc280270c8a4efb18daaef95
mbitu <mbitumutonga@gmail.com>
2021-11-29 17:29:32 +0300

    c program codes

--- main.c
+++ main.c
@@ -3,6 +3,17 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int x;
+    printf("Enter integer from the keyboard\n");
+    scanf("%d",&x);
+    if(x%2==0)
+    {
+    printf("%d is  an odd number",x);
+    }
+
+    else if(x%2==1)
+    {
+    printf("%d is a even number",x);
+    }
     return 0;
 }
